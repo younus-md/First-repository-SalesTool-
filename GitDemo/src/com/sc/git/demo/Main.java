00a4cdda02e436dae94a037a4d99ef6554108f9c
yns <smdyns97@gmail.com>
2016-06-01 19:34:25 +0530

    Initial Check

---
+++ GitDemo/src/com/sc/git/demo/Main.java
@@ -0,0 +1,18 @@
+package com.sc.git.demo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesData sData = new SalesData();
+		sData.display();
+		greetHello();
+	}
+
+	private static void greetHello() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello User,Welcome 2 Git");
+
+	}
+
+}
---
+++ GitDemo/src/com/sc/git/demo/SalesData.java
@@ -0,0 +1,9 @@
+package com.sc.git.demo;
+
+public class SalesData {
+	int arrayElemnts[] = { 40, 50, 60 };
+
+	public void display() {
+		System.out.println("Inside SalesData Class");
+	}
+}
